a962243aaf2ab08215336a312d2c08b802fab233
Vitor Machado <vitoralmeida.work@gmail.com>
2023-03-13 10:39:23 -0300

    Initial setup

---
+++ target/debug/deps/zero2prod-b06df8c0ff68baf2.d
@@ -0,0 +1,5 @@
+c:\Users\vitor\Documents\GitHub\zero2prod\zero2prod\target\debug\deps\zero2prod-b06df8c0ff68baf2.rmeta: src\main.rs
+
+c:\Users\vitor\Documents\GitHub\zero2prod\zero2prod\target\debug\deps\zero2prod-b06df8c0ff68baf2.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/zero2prod-e1b900cf4c9f1364.d
@@ -0,0 +1,5 @@
+c:\Users\vitor\Documents\GitHub\zero2prod\zero2prod\target\debug\deps\zero2prod-e1b900cf4c9f1364.rmeta: src\main.rs
+
+c:\Users\vitor\Documents\GitHub\zero2prod\zero2prod\target\debug\deps\zero2prod-e1b900cf4c9f1364.d: src\main.rs
+
+src\main.rs:
