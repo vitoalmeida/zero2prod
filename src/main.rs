2311420dbee92f8b763077a5b219be35a9d6fe3a
Vitor M <vitoralmeida.work@gmail.com>
2023-03-17 13:27:50 -0300

    test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use secrecy::ExposeSecret;
 use sqlx::postgres::PgPoolOptions;
-use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -14,7 +12,7 @@ async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
         .connect_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(&configuration.database.with_db())
+        .connect_lazy_with(configuration.database.with_db())
         .expect("Failed to connect to Postgres.");
     let address = format!(
         "{}:{}",
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -73,21 +73,21 @@ async fn spawn_app() -> TestApp {
 pub async fn configure_database(config: &DatabaseSettings) -> PgPool {
     // Create database
     let mut connection = PgConnection::connect_with(&config.without_db())
-    .await
-    .expect("Failed to connect to Postgres");
+        .await
+        .expect("Failed to connect to Postgres");
     connection
-    .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
-    .await
-    .expect("Failed to create database.");
-    
+        .execute(format!(r#"CREATE DATABASE "{}";"#, config.database_name).as_str())
+        .await
+        .expect("Failed to create database.");
+
     let connection_pool = PgPool::connect_with(config.with_db())
-.await
-.expect("Failed to connect to Postgres.");
-sqlx::migrate!("./migrations")
-.run(&connection_pool)
-.await
-.expect("Failed to migrate the database");
-connection_pool
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&connection_pool)
+        .await
+        .expect("Failed to migrate the database");
+    connection_pool
 }
 
 #[tokio::test]
