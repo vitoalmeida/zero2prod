12f0e5984d8315e55181de25f87c1903a6ea284d
Vitor M <vitoralmeida.work@gmail.com>
2023-03-21 11:03:36 -0300

    sql migrations

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use sqlx::postgres::PgPoolOptions;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -10,14 +8,9 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .connect_timeout(std::time::Duration::from_secs(2))
-        // `connect_lazy_with` instead of `connect_lazy`
-        .connect_lazy_with(configuration.database.with_db());
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-    let listener = TcpListener::bind(address)?;
-    run(listener, connection_pool)?.await
+
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+
+    Ok(())
 }
