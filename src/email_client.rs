5412ed30ad909c7bee2a25541a57fbfe49000d42
Vitor M <vitoralmeida.work@gmail.com>
2023-03-24 10:06:45 -0300

    end of chapter

--- src/email_client.rs
+++ src/email_client.rs
@@ -27,7 +27,7 @@ impl EmailClient {
 
     pub async fn send_email(
         &self,
-        recipient: SubscriberEmail,
+        recipient: &SubscriberEmail,
         subject: &str,
         html_content: &str,
         text_content: &str,
@@ -143,7 +143,7 @@ mod tests {
             .await;
         // Act
         let _ = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
         // Assert
         // Mock expectations are checked on drop
@@ -167,7 +167,7 @@ mod tests {
             .await;
         // Act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
         // Assert
         assert_ok!(outcome);
@@ -195,7 +195,7 @@ mod tests {
             .await;
         // Act
         let outcome = email_client
-            .send_email(subscriber_email, &subject, &content, &content)
+            .send_email(&subscriber_email, &subject, &content, &content)
             .await;
         // Assert
         assert_err!(outcome);
@@ -217,7 +217,7 @@ mod tests {
             .await;
         // Act
         let outcome = email_client
-            .send_email(email(), &subject(), &content(), &content())
+            .send_email(&email(), &subject(), &content(), &content())
             .await;
         // Assert
         assert_err!(outcome);
